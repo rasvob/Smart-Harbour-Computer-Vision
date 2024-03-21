65ff7cb24bf275c0a79135c99ca2e13895fe84d9
Radek Svoboda <rasvob14@gmail.com>
2024-03-21 15:00:47 +0100

    Test API script added

---
+++ object-detection/utils/README.md
@@ -0,0 +1,7 @@
+# Inference time
+
+| Model | Average Time (ms) |
+|-----------|-------------|
+| YOLOv8x - RT - HalfP - RTX 3090  | 84.6 |
+| YOLOv8x - RT - HalfP - RTX 4080  | xxx |
+
--- object-detection/utils/test_api_on_video_file.py
+++ object-detection/utils/test_api_on_video_file.py
@@ -3,13 +3,29 @@ import logging
 import cv2
 import base64
 import requests
+import json
+import time
 from tqdm import tqdm
 from dotenv import load_dotenv
 from video_stream import VideoStream
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+times = []
 
+# Create decorator for measuring time in miliseconds, store times in array and return average time
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        global times
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        times.append((end - start) * 1000)
+        logger.debug(f'Execution time: {(end - start) * 1000} ms')
+        return result
+    return wrapper
+
+@measure_time
 def send_frame(image, endpoint):
     encoded_image = base64.b64encode(image).decode('utf-8')
     headers = {'Content-Type': 'application/json'}
@@ -22,9 +38,12 @@ if __name__ == "__main__":
     params = {
         'input-file': os.environ.get("INPUT_FILE"),
         'video-width': int(os.environ.get("VIDEO_WIDTH")),
+        'api-endpoint': os.environ.get("ENDPOINT"),
     }
 
     video = VideoStream(params['input-file'])
-    for i, frame in tqdm(enumerate(video)):
+    for frame in tqdm(video, total=len(video)):
         ret, buffer = cv2.imencode('.jpg', frame)
-        send_frame
+        res = send_frame(buffer, params['api-endpoint'])
+
+    print(f'Average time: {sum(times) / len(times)} ms')
